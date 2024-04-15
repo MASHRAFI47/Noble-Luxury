5093c11dc90f4d155e5f351d9fe273db012b75f0
MASHRAFI47 <mbngms@gmail.com>
2024-04-15 23:54:08 +0600

    update profile page added

--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -1,8 +1,62 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
 
 const UpdateProfile = () => {
-  return (
-    <div>UpdateProfile</div>
-  )
+    const [showPass, setShowPass] = useState(false)
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => {
+       console.log(data)
+    }
+    return (
+        <div className="card shrink-0 w-full max-w-lg shadow-2xl bg-base-100 mx-auto border">
+            <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Full Name</span>
+                    </label>
+                    <input type="text" placeholder="full name" className="input input-bordered" {...register("fullName", { required: true })} />
+                    {errors.fullName && <span>This field is required</span>}
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Photo Url</span>
+                    </label>
+                    <input type="text" placeholder="photo url" className="input input-bordered" {...register("photoURL", { required: true })} />
+                    {errors.photoURL && <span>This field is required</span>}
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" placeholder="email" className="input input-bordered" {...register("email", { required: true })} />
+                    {errors.email && <span>This field is required</span>}
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type={showPass ? "text" : "password"} placeholder="password" className="input input-bordered" {...register("password", { required: true })} />
+                    <span className="mt-0 mr-0 ml-auto mb-0 relative bottom-8 right-4" onClick={() => setShowPass(!showPass)}>
+                        {
+                            showPass ? <FaEye /> : <FaEyeSlash />
+                        }
+                    </span>
+                    {errors.password && <span>This field is required</span>}
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Register</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default UpdateProfile
