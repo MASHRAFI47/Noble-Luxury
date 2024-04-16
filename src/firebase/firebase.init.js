eabea6afcbb4a7b0e2a0cdaab49a8e5a1706ab47
MASHRAFI47 <mbngms@gmail.com>
2024-04-16 19:04:14 +0600

    firebase keys hidden

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,13 +5,14 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log()
 const firebaseConfig = {
-  apiKey: "AIzaSyATS4MCxrduNWAfr5Mj-oHsK3VdaN9hKKs",
-  authDomain: "noble-luxury.firebaseapp.com",
-  projectId: "noble-luxury",
-  storageBucket: "noble-luxury.appspot.com",
-  messagingSenderId: "598025064883",
-  appId: "1:598025064883:web:16215b08d823448e31bbc6"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
